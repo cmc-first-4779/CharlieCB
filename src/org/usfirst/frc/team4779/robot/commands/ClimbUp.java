51d3abc8d5831032ac4206f693d0c2298583444c
Kevin <Kevin>
2018-01-26 06:24:16 -0500

    First Commit

---
+++ src/org/usfirst/frc/team4779/robot/commands/ClimbUp.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team4779.robot.commands;
+
+import org.usfirst.frc.team4779.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ClimbUp extends Command {
+
+    public ClimbUp() {
+    	requires(Robot.climber);
+   
+    	
+    	
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.climber.climbDoNothing();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.climber.climbUp();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+       // return isTimedOut();
+    	return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team4779/robot/subsystems/Climber.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team4779.robot.subsystems;
+
+import org.usfirst.frc.team4779.robot.RobotMap;
+import org.usfirst.frc.team4779.robot.commands.ClimberDoNothing;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/** 
+ *
+ */
+public class Climber extends Subsystem {
+
+ Spark climbMotor = new Spark(RobotMap.climbMotor);
+ 
+ public Climber() {
+	 super();
+ }
+
+    public void initDefaultCommand() {
+    	setDefaultCommand(new ClimberDoNothing());    
+    }
+    
+    public void climbUp() {
+    	climbMotor.set(-1);	
+    }
+    
+    public void climbDown() {
+    	climbMotor.set(1);
+    }
+    
+    public void climbDoNothing() {
+    	climbMotor.set(0);	
+    }
+    
+	public void log() {
+	}
+}
+
