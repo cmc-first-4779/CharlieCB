5d751deba39b0c79c8d0e34601542fcde9b5b8af
pkparlow <pkparlow@10.0.1.188>
2018-01-31 17:27:57 -0500

    Here is the shoot.java code.

---
+++ src/org/usfirst/frc/team4779/robot/commands/Shoot.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team4779.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Shoot extends CommandGroup {
+
+    public Shoot() {
+    	addSequential(new ShooterOn(2));
+    	addParallel(new CarouselOn());
+    	addParallel(new ShooterOn(10));
+    }
+}
